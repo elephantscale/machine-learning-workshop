afd106680ab900c4b4e0247985b16026a2c0888c
Sujee Maniyam <sujee@elephantscale.com>
2021-09-30 18:21:19 -0700

    added streamlit demos

---
+++ streamlit/README.md
@@ -0,0 +1,24 @@
+# Streamlit Demos
+
+## Install Streamlit
+
+```bash
+	$  pip install streamlit
+```
+
+## Run
+
+```bash
+	$  streamlit run <file.py>
+```
+
+## Demos
+
+### Uber Pickups in NYC
+
+```bash
+	$  streamlit  run uber.py
+```
+
+Go to URL printed out, usually http://localhost:8501
+
---
+++ streamlit/nycflights.py
@@ -0,0 +1,31 @@
+## TODO: work in progress
+
+import streamlit as st
+import pandas as pd
+
+# get data
+import os
+import urllib.request
+
+# download data
+data_location = "flights.csv.gz"
+data_url = 'https://elephantscale-public.s3.amazonaws.com/data/nycflights13/flights.csv.gz'
+
+if not os.path.exists (data_location):
+    data_location = os.path.basename(data_location)
+    if not os.path.exists(data_location):
+        urllib.request.urlretrieve(data_url, data_location)
+        print ('Downloading : ', data_url)
+print('data_location :', data_location)  
+
+
+# load data
+flights_all = pd.read_csv(data_location)
+flights = flights_all.sample(10000)
+
+dest = flights['dest'].unique()
+dest_select = st.multiselect("Choose Destination", dest)
+
+flights_by_dest = flights[flights['dest'] == 'ORD']
+
+st.write("# Flights Data", flights_by_dest)
