b4e790604f64a5714bbd193048dd42573ad77584
Sujee Maniyam <sujee@elephantscale.com>
2022-06-08 18:18:34 -0700

    model serving

---
+++ model-serving/README.md
@@ -0,0 +1,66 @@
+# Model Serving
+
+## Step-1: Build and Save a Model
+
+Run [build-model.ipynb](build-model.ipynb) to build and save a model
+
+## WebService
+
+We will host the service using [Flask web server](https://flask.palletsprojects.com/en/1.1.x/)
+
+## Step-2: Install 
+
+Install prerequisites
+
+To run the webservices, install the prerequisites
+
+```bash
+
+# for conda environments
+$  conda  install flask
+# or
+# for pip install
+$  pip install flask
+```
+
+## Step-3: web_service.py
+
+Inspect file [web_service.py](web_service.py)
+
+
+## Step-4: Run the webservice
+
+```bash
+$   cd  model-serving
+$   python web_service.py
+```
+
+## Step-5: Make a Web service request
+
+We will use `curl` as our client
+
+```bash
+$  curl 'http://localhost:5000/'
+```
+
+We will see 
+
+```text
+Hello world!
+```
+
+Great, web service is working
+
+## Step-6 : Call `prediction` webservice
+
+Run the following request.  You can modify the parameters to test it out
+
+```bash
+$   curl 'http://localhost:5000/predict?Bedrooms=4&Bathrooms=2.5&SqFtTotLiving=2000&SqFtLot=8000&LandVal=500000'
+```
+
+## Production ready web services
+
+Here we used [Flask web server](https://flask.palletsprojects.com/en/1.1.x/) for a small web service.  While Flask is really great for quick deployments, it is not really production strength.
+
+Look at frameworks like [gunicorn](https://gunicorn.org/) for implementing production ready systems.
---
+++ model-serving/web_service.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify
+import joblib
+from sklearn.ensemble import GradientBoostingRegressor
+
+app = Flask(__name__)
+
+# load model
+model = joblib.load("model.pkl")
+print ("Loaded model:", model)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+
+    print ('request received:\n', request.args)
+    result = {}
+    prediction = -1
+    
+    try:
+        bedrooms = int (request.args.get('Bedrooms'))
+        bathrooms = float (request.args.get('Bathrooms'))
+        sqft = int (request.args.get('SqFtTotLiving'))
+        lot_size = int (request.args.get('SqFtLot'))
+        land_val = int (request.args.get('LandVal'))
+
+        columns = ["Bedrooms", "Bathrooms", "SqFtTotLiving", "SqFtLot", "LandVal"]
+        data = [(bedrooms, bathrooms, sqft, lot_size, land_val)]
+        #print ("Received data: ", data)
+        
+        prediction = model.predict(data)
+        print ('predicted price ', prediction[0])
+        result = {
+            'prediction' :prediction[0],
+        }
+
+
+    except :
+        prediction = -1.0
+        
+        result = {
+            'prediction' : prediciton,
+            'error' : 'some thing went wrong'
+        }
+    ## end try-except block
+        
+    return jsonify(result)
+
+        
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
+    #app.run()
+
